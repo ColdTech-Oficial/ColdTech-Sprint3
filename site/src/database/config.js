b7bf61df93e75116e44ea014b08dada6326cc908
HeinrickVHO2 <victor.oliveira@sptech.school>
2022-05-22 21:38:24 -0300

    alterações gramaticais e no gráfico da calculadora

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    user: "aluno",
+    user: "root",
     database: "coldtech",
-    password: "sptech",
+    password: "VICtor2018**",
 };
 
 function executar(instrucao) {
